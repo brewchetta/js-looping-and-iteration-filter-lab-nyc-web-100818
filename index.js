1abb38ef6a2654133cd0c635eb069d2857c135a2
brewchetta <chett.tiller@gmail.com>
2018-11-19 10:21:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file
+function findMatching(a, name){
+  return a.filter(function(n){
+    return n.toLowerCase() == name.toLowerCase();
+    });
+};
+
+function fuzzyMatch(a, s){
+  return a.filter(function(n){
+    return n.toLowerCase().startsWith(s.toLowerCase())
+  });
+};
+
+function matchName(a, s){
+  return a.filter(function(n){
+    return n.name == s
+  });
+};
